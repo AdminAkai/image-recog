5c18c3ac070ced048a609950214ee7c1ba3f7c27
JCaptainnnn <49946613+JCaptainnnn@users.noreply.github.com>
2019-12-03 00:15:03 -0500

    rendering all bounding box for image recog

--- client/src/components/facerecognition/FaceRecognition.js
+++ client/src/components/facerecognition/FaceRecognition.js
@@ -4,11 +4,22 @@ import './FaceRecognition.css'
 export default class FaceRecognition extends Component {
 
     render() {
+    
+    const allBorders = this.props.boxArea.map(area => {
+        <div className='bounding-box' style={{
+            top: area.topRow, 
+            right: area.rightCol, 
+            bottom: area.bottomRow, 
+            left: area.leftCol}}
+        ></div>
+    })   
+
+
         return(
             <div className='center ma container'>
                 <div className="absolute mt4">
                     <img alt='' id='input-image' src={this.props.imageUrl} width="500px" height="auto"></img>
-                    <div className='bounding-box' style={{top: this.props.boxArea.topRow, right: this.props.boxArea.rightCol, bottom: this.props.boxArea.bottomRow, left: this.props.boxArea.leftCol}}></div>
+                    {allBorders}
                 </div>
             </div>
         )
